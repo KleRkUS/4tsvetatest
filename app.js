9246c45cfcafec6aa63e8e7d2d03a710158d4e66
KleRkUS <villevald.vladislav.sleep@gmail.com>
2020-03-20 17:34:16 +0300

    edit port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express'),
       app = express(),
       path = require("path"),
-      port = 8080,
+      port = 80,
       index = require('./app/routes/index.js');
 
 app.use(express.static(path.join(__dirname, '/public')));
